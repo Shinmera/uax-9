3802fcaee72a5ee51e6bfc84eb64c5c4f07b9e44
Shinmera <shinmera@tymoon.eu>
2019-09-12 19:04:47 +0200

    Docgen

---
+++ staple.ext.lisp
@@ -0,0 +1,17 @@
+(asdf:load-system :staple-markless)
+
+(defpackage "uax-9-docs"
+  (:use #:cl)
+  (:local-nicknames
+   (#:uax-9 #:org.shirakumo.alloy.uax-9)))
+
+(defclass page* (staple:simple-page)
+  ()
+  (:default-initargs :document-package (find-package "uax-9-docs")))
+
+(defmethod staple:subsystems ((system (eql (asdf:find-system :uax-9))))
+  ())
+
+(defmethod staple:page-type ((system (eql (asdf:find-system :uax-9))))
+  'page*)
+
